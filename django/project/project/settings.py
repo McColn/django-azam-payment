efe0457b61063c75e13c8531c9fd402377fc9eb2
McColn <102020324+McColn@users.noreply.github.com>
2025-03-03 15:47:29 +0300

    Update settings.py

--- django/project/project/settings.py
+++ django/project/project/settings.py
@@ -134,10 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     # "MERCHANT_ACCOUNT": "your_merchant_account",
 # }
 
-AZAMPAY = {  
-    "BASE_URL": "https://sandbox.azampay.co.tz",
-    "appName": "McColn",
-    "clientId": "f5f66619-bfd8-4375-8b79-0b7caa642692",
-    "clientSecret": "FZpjnMnIUf/C5n3e9xk2R2t48eTM4qvdb53f1Cnl7CqO2ojDMfVu9Udsx6YJiHKtRxbsKfVuRoA8oetTGOzD13GtyfVbYthZdzrS7s8hWwXP9lnWRR13EPE3Tf04hU98aynXbgZ5mZkvGGwNTOUI8yCcVfEimTCgMIgRYOCXD+VJ3Gwqlgi4kIoDz9z7eRPnOlTJbRaNDjezU5uX0GaM2vx3mHECsA/KoPPSNFlTE6UXIZJKXSuPxaaYxwMs0QmlLcx6sIh3mJeua7Gg7kdRs0kWzI6WYDHn2vivYzxrY+Di7HFn5j1y9fS8kQYAEbGipjyXGLzFK2/FBn4L2xT+fdVBgRFmGPp4XvVttTL7FOUNibl3l2SuzYq0ddIlTAde108xI8vgE7ln6umyRBI/9gJloVaMbGHwlQHuxVeZF3NYGQEWvIBjK9Wzazha/tUAjx7vaCuM7v/CgUb6Ml/2uDS7e+/UAbY0+tHiHfHXQ9FRrLCd1+aI08idflbEM0xSqeXVOIZ9JTR/J2PogRRCa5yoO7sGyok868kyghQBoKKYoMzukgOyvgAszBhGE34pHALF2aKC2a/mRR5/9AqhNf/yPhI246Tqg7fb1NE+zc52wBFKJTU6UcdVSkgLtuABsZjNPKSKLlfLfX9wmtcuW7cPF5uN5pFEaX7tycgzeEk="
-}
+
 
